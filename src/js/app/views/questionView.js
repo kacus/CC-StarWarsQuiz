3caeef000ad5fa6268e24aa732b1a53a0ebda162
Kinetic639 <michal.stepien36@gmail.com>
2021-01-02 22:01:17 +0100

    binding answerHandler to view

--- src/js/app/views/questionView.js
+++ src/js/app/views/questionView.js
@@ -1,62 +1,63 @@
 export default class QuestionView {
-    constructor(parent, answerHandler, type) {
-        this.parent = parent;
-        this.answerHandler = answerHandler;
-        this.type = type
-        this.questionText = `Question: ${this.getQuestionText(type)}`
+  constructor(parent, answerHandler, type) {
+    this.parent = parent;
+    this.answerHandler = answerHandler;
+    this.type = type;
+    this.questionText = `Question: ${this.getQuestionText(type)}`;
+  }
+
+  getQuestionText(type) {
+    switch (type) {
+      case 'people':
+        return 'Who is this character?';
+      case 'vehicles':
+        return 'What vehicle is that?';
+      case 'starships':
+        return 'What starship is that?';
+      default:
+        return 'Wrong type selected';
     }
+  }
 
-    getQuestionText(type) {
-        switch (type) {
-            case 'people':
-                return 'Who is this character?';
-            case 'vehicles':
-                return 'What vehicle is that?';
-            case 'starships':
-                return 'What starship is that?';
-            default:
-                return 'Wrong type selected'
-        }
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    return element;
+  }
 
+  shuffleAnswers(array) {
+    const shuffledAnswers = [...array];
+    for (let i = shuffledAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledAnswers[i], shuffledAnswers[j]] = [
+        shuffledAnswers[j],
+        shuffledAnswers[i],
+      ];
     }
+    return shuffledAnswers;
+  }
 
-    // Create an element with an optional CSS class
-    createElement(tag, className) {
-        const element = document.createElement(tag)
-        if (className) element.classList.add(className)
-        return element
-    }
-
-    shuffleAnswers(array) {
-        const shuffledAnswers = [...array]
-        for (let i = shuffledAnswers.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffledAnswers[i], shuffledAnswers[j]] = [shuffledAnswers[j], shuffledAnswers[i]];
-        }
-        return shuffledAnswers
-    }
-
-    display(answers) {
-        const shuffledAnswers = this.shuffleAnswers(answers)
-        const questionWrapper = this.createElement('div', 'question__wrapper');
-        const questionImage = this.createElement('img', 'question__image');
-        const questionContainer = this.createElement('div', 'question__container');
-        const questionText = this.createElement('h1', 'question__text');
-        const answersList = this.createElement('div', 'answers__list');
-        shuffledAnswers.forEach(answer => {
-            const answerButton = this.createElement('button', 'answer__btn');
-            answerButton.innerText = answer[1];
-            answerButton.classList.add('btn')
-            answersList.appendChild(answerButton)
-
-        });
-        questionText.innerText = this.questionText
-        questionImage.src = `./static/assets/img/modes/${this.type}/${answers[0][0]}.jpg`;
-        // this.parent.appendChild(questionWrapper)
-        this.parent.appendChild(questionImage)
-        this.parent.appendChild(questionContainer)
-        questionContainer.appendChild(questionText)
-        questionContainer.appendChild(answersList)
-    }
-
+  display(answers) {
+    const shuffledAnswers = this.shuffleAnswers(answers);
+    const questionImage = this.createElement('img', 'question__image');
+    const questionContainer = this.createElement('div', 'question__container');
+    const questionText = this.createElement('h1', 'question__text');
+    const answersList = this.createElement('div', 'answers__list');
+    shuffledAnswers.forEach((answer) => {
+      const answerButton = this.createElement('button', 'answer__btn');
+      answerButton.innerText = answer[1];
+      answerButton.classList.add('btn');
+      answerButton.addEventListener('click', () =>
+        this.answerHandler(answer[0]),
+      );
+      answersList.appendChild(answerButton);
+    });
+    questionText.innerText = this.questionText;
+    questionImage.src = `./static/assets/img/modes/${this.type}/${answers[0][0]}.jpg`;
+    this.parent.appendChild(questionImage);
+    this.parent.appendChild(questionContainer);
+    questionContainer.appendChild(questionText);
+    questionContainer.appendChild(answersList);
+  }
 }

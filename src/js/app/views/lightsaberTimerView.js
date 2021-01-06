5df2b7990b027910554779cec9def26405f5e106
pkol92 <paulina_kolasa@wp.pl>
2021-01-06 02:16:45 +0100

    code review changes

--- src/js/app/views/lightsaberTimerView.js
+++ src/js/app/views/lightsaberTimerView.js
@@ -1,36 +1,49 @@
 class LightsaberTimerView {
     constructor(parent) {
-        this.parent = parent
+        this.parent = parent;
     }
 
     createElement(tag, className) {
-        const element = document.createElement(tag)
-        if(className) element.className.add(className)
+        const element = document.createElement(tag);
+        if (className) element.classList.add(className);
         return element;
     }
 
     display(timeLeft, maxTime) {
         //create box 
-        this.timerView = this.createElement('div','TimerView')
-        //create lightsaber and path
-        this.lightsaber = this.createElement('div','lightsaber')
-        this.pathLightsaber = this.createElement('div', 'pathLightsaber')
-        this.lightsaber.append(this.pathLightsaber)
-        //create countingdown text
-        this.progress = this.createElement('progress')
-        this.timerView.append(this.lightsaber, this.progress)
-        this.parent.append(this.timerView)
+        this.timerView = this.createElement('div','TimerView');
+        //create lightsaber 
+        this.lightsaber = this.createElement('div','lightsaber');
+        //create progress and label
+        this.progress = this.createElement('progress');
+        this.setProgressAtributes(this.progress, timeLeft, maxTime);
+        this.progress.id = "lightsaberTimer";
+        this.label = this.createElement('label');
+        this.label.setAttribute('for', `lightsaberTimer`);
+        this.label.textContent = "Time left: ";
+        this.spanLabel = this.createElement('span');
+        this.label.append(this.spanLabel);
+
+        this.timerView.append(this.lightsaber, this.progress, this. label);
+        this.parent.append(this.timerView);
+        this.updateTime(timeLeft, maxTime);
     }
 
-    updateTimer(timeLeft, maxTime) {
-        this.maxTime = maxTime
-        this.timeLeft = timeLeft
+    updateTime(timeLeft, maxTime) {
+        this.maxTime = maxTime;
+        this.timeLeft = timeLeft;
 
-        const minutes = Math.floor(timeLeft/60)
-        const seconds = timeLeft - (minutes*60)
+        const minutes = Math.floor(timeLeft/60);
+        const seconds = timeLeft - (minutes*60);
 
-        this.progress.textContent = `Time left: ${minutes}m ${seconds}s`
+        this.spanLabel.textContent = `${minutes}m ${seconds}s`;
+        this.setProgressAtributes(this.progress, timeLeft, maxTime);
+    }
 
-        this.pathLightsaber.style.width = `(${timeLeft}*100/${maxTime})%`
+    setProgressAtributes(element, value, max) {
+        element.setAttribute('value', value);
+        element.setAttribute('max', max);
     }
 }
+
+export default LightsaberTimerView;

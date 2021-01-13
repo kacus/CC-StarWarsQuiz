dbf8aa3598761df14c46dc6c6dfbe7a84344fcec
cwaniakobezczel <70508434+cwaniakobezczel@users.noreply.github.com>
2021-01-13 19:17:39 +0100

    Fix submitNameHandler and add static elements to the view

--- src/js/app/views/gameOverView.js
+++ src/js/app/views/gameOverView.js
@@ -1,30 +1,57 @@
 export default class GameOverView {
-    constructor(parent,submitNameHandler){
+    constructor(parent, submitNameHandler,correctAnswers, totalAnswers) {
         this.parent = parent;
-        this.submitNameHandler = submitNameHandler();
+        this.submitNameHandler = submitNameHandler;
+        this.correctAnswers = correctAnswers;
+        this.totalAnswers = totalAnswers;
+
     }
 
-    createElement(tag, className){
+    createElement(tag, className) {
         const element = document.createElement(tag)
         if (className) element.classList.add(className)
 
         return element
     }
 
-    display(){
-        const formContainer = this.createElement('div','form__container');
-        const nameForm = this.createElement('form','name__form');
+    display() {
+        const formContainer = this.createElement('div', 'form__container');
+        const nameForm = this.createElement('form', 'name__form');
         const insertName = this.createElement('input', 'insert__name');
-        insertName.type='text';
-        insertName.setAttribute('maxlength','20');
-        insertName.setAttribute('pattern','[a-zA-Z0-9]')
-        const submitButton = this.createElement('input', 'submit__button');
-        submitButton.type='submit';
-        submitButton.value='MAY THE FORCE BE WITH YOU!';
-        submitButton.addEventListener('click', ()=>this.submitNameHandler());
+        insertName.type = 'text';
+        insertName.setAttribute('maxlength', '20');
+        insertName.setAttribute('pattern', '[a-zA-Z0-9]{3,20}');
+        const submitButton = this.createElement('button', 'submit__button');
+        submitButton.innerText = 'MAY THE FORCE BE WITH YOU!';
+        submitButton.addEventListener('click', () => {
+            let providedName = insertName.value;
+            this.submitNameHandler(providedName)
+        });
+
+
+      
+        const headerContainer = this.createElement('div', 'header__container');
+        const gameOverHeader = this.createElement('h2', 'gameover__header');
+        gameOverHeader.innerText = "GAME OVER";
+        const gameOverSubheader = this.createElement('h5', 'subheader');
+        gameOverSubheader.innerText = `The force is strong in you young Padawan! During 1 minute you have answered ${this.correctAnswers} / ${this.totalAnswers} questions.`
+        const yodaImage = this.createElement('img', 'yoda__image');
+        yodaImage.src = `./static/assets/ui/MasterYodaRIght.png`;
+        const formHolder = this.createElement('div', 'form__holder');
+        const provideNameText = this.createElement('h5', 'provideName__text');
+        provideNameText.innerText = "Please fill your name in order to receive eternal glory in whole Galaxy!"
+
+
+    
 
         this.parent.appendChild(formContainer);
-        formContainer.appendChild(nameForm);
+        formContainer.appendChild(headerContainer);
+        formContainer.appendChild(yodaImage);
+        formContainer.appendChild(formHolder);
+        formHolder.appendChild(provideNameText);
+        formHolder.appendChild(nameForm);
+        headerContainer.appendChild(gameOverHeader);
+        headerContainer.appendChild(gameOverSubheader);
         nameForm.appendChild(insertName);
         nameForm.appendChild(submitButton);
     }
